917f4cebbdca4bf220aea29965923d578ba48b3d
kowsikreddy <kowsikreddy@gmail.com>
2020-07-08 14:14:02 -0500

    7/81

--- public/js/index.js
+++ public/js/index.js
@@ -36,9 +36,78 @@ function getIPHtml(data){
     // div.innerHTML = "IP Address = "+data.text;
     // mainContainer.appendChild(div); 
     
-  
-
 }
+
+function search_name() {
+  console.log("get_data");
+  // var a1 = document.forms["form1"]["a1"].value;
+  // var a2 = document.forms["form1"]["a2"].value;
+  // var a3 = document.forms["form1"]["a3"].value;
+    fetch('/get_data', 
+    {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      //body: JSON.stringify({a1: a1})
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+      // console.log("in getIPF data = ",data)
+      // getIPHtml(data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    }); 
+    return false;
+}
+
+function search_name() {
+  console.log("get_data");
+  // var a1 = document.forms["form1"]["a1"].value;
+  // var a2 = document.forms["form1"]["a2"].value;
+  // var a3 = document.forms["form1"]["a3"].value;
+    fetch('/get_data', 
+    {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      //body: JSON.stringify({a1: a1})
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+      // console.log("in getIPF data = ",data)
+      // getIPHtml(data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    }); 
+    return false;
+}
+
+function findFreuencyf() {
+  console.log("findFreuencyf");
+  // var a1 = document.forms["form1"]["a1"].value;
+  // var a2 = document.forms["form1"]["a2"].value;
+  // var a3 = document.forms["form1"]["a3"].value;
+    fetch('/most_frequent', 
+    {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      //body: JSON.stringify({a1: a1})
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+      // console.log("in getIPF data = ",data)
+      // getIPHtml(data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    }); 
+    return false;
+}
+
+
 //*******************Form1 Function ********************************
   function form1f() {
     console.log("form1");
@@ -311,152 +380,6 @@ function form3html(data) {
 //      var div = document.createElement("div");
 //      div.style.padding = '20px';
 //      div.innerHTML = (i+1)+". Year = "+ data[i].year.value +"; Number of Terrorist Incidents = "+ data[i].NumberTerroristIncidents.value+"; Country Name = "+ data[i].Entity.value;
-//      mainContainer.appendChild(div); 
-//    }
- 
-//  }
-
-//  // *******************quiz3 Form 3 Function ********************************
-// function form3q3f() {
-//    var a1 = document.forms["form3q3"]["a1"].value;
-//    var a2 = document.forms["form3q3"]["a2"].value;
-   
-//    if (a1 == "" || a2=="" ) {
-//      alert("All entries must be filled");
-//      return false;
-//    }
-//    else{
-//      fetch('/form3q3', 
-//      {
-//        method: 'POST',
-//        headers: {'Content-Type': 'application/json'},
-//        body: JSON.stringify({a1: a1,a2:a2})
-//      })
-//      .then(response => response.json())
-//      .then(data => {
-//        console.log('Success:', data);
-//        form3q3html(data);
-//      })
-//      .catch((error) => {
-//        console.error('Error:', error);
-//      }); 
-//      return false;
-//    }
-//  }
- 
-//  //******************* quiz3 Form 3 Function append html elements********************************
-//  function form3q3html(data) {
-//    console.log("data in function",data)
-//    document.getElementById("myData").innerHTML = "";
-//    var mainContainer = document.getElementById("myData");
-   
-//    var div = document.createElement("div");
-//      div.style.padding = '20px';
-//      div.innerHTML = "The time to do this query = " + data[data.length-1].timeTaken+ " ms";
-//      mainContainer.appendChild(div); 
- 
-//    for (var i = 0; i < data.length-1; i++) {
-//      var div = document.createElement("div");
-//      div.style.padding = '20px';
-//      div.innerHTML = (i+1)+". Count = "+ data[i].count.value +"; Country Name = "+ data[i].name.value;
-//      mainContainer.appendChild(div); 
-//    }
- 
-//  }
-
-//  // *******************quiz3 Form 4 Function ********************************
-// function form4q3f() {
-//    var a1 = document.forms["form4q3"]["a1"].value;
-//    var a2 = document.forms["form4q3"]["a2"].value;
-//    var a3 = document.forms["form4q3"]["a3"].value;
-
-//    if (a1 == "" || a2=="" ||a3 =="") {
-//      alert("All entries must be filled");
-//      return false;
-//    }
-//    else{
-//      fetch('/form4q3', 
-//      {
-//        method: 'POST',
-//        headers: {'Content-Type': 'application/json'},
-//        body: JSON.stringify({a1: a1,a2:a2,a3:a3})
-//      })
-//      .then(response => response.json())
-//      .then(data => {
-//        console.log('Success:', data);
-//        form4q3html(data);
-//      })
-//      .catch((error) => {
-//        console.error('Error:', error);
-//      }); 
-//      return false;
-//    }
-//  }
- 
-//  //******************* quiz3 Form 4 Function append html elements********************************
-//  function form4q3html(data) {
-//    console.log("data in function",data)
-//    document.getElementById("myData").innerHTML = "";
-//    var mainContainer = document.getElementById("myData");
-   
-//    var div = document.createElement("div");
-//      div.style.padding = '20px';
-//      div.innerHTML = "The total time taken = " + data[data.length-1].totalTime+ " ms";
-//      mainContainer.appendChild(div); 
- 
-//    for (var i = 0; i < data.length-1; i++) {
-//      var div = document.createElement("div");
-//      div.style.padding = '20px';
-//      div.innerHTML = "Time taken for query "+(i+1)+" = "+ data[i].timeTaken+" ms";
-//      mainContainer.appendChild(div); 
-//    }
- 
-//  }
-
-//  // *******************quiz3 Form 5 Function ********************************
-// function form5q3f() {
-//    var a1 = document.forms["form5q3"]["a1"].value;
-//    var a2 = document.forms["form5q3"]["a2"].value;
-//    var a3 = document.forms["form5q3"]["a3"].value;
-   
-//    if (a1 == "" || a2=="" ||a3 == "") {
-//      alert("All entries must be filled");
-//      return false;
-//    }
-//    else{
-//      fetch('/form5q3', 
-//      {
-//        method: 'POST',
-//        headers: {'Content-Type': 'application/json'},
-//        body: JSON.stringify({a1: a1,a2:a2,a3:a3})
-//      })
-//      .then(response => response.json())
-//      .then(data => {
-//        console.log('Success:', data);
-//        form5q3html(data);
-//      })
-//      .catch((error) => {
-//        console.error('Error:', error);
-//      }); 
-//      return false;
-//    }
-//  }
- 
-//  //******************* quiz3 Form 3 Function append html elements********************************
-//  function form5q3html(data) {
-//    console.log("data in function",data)
-//    document.getElementById("myData").innerHTML = "";
-//    var mainContainer = document.getElementById("myData");
-   
-//    var div = document.createElement("div");
-//      div.style.padding = '20px';
-//      div.innerHTML = "The total time taken = " + data[data.length-1].totalTime+ " ms";
-//      mainContainer.appendChild(div); 
- 
-//    for (var i = 0; i < data.length-1; i++) {
-//      var div = document.createElement("div");
-//      div.style.padding = '20px';
-//      div.innerHTML = "Time taken for query "+(i+1)+" = "+ data[i].timeTaken+" ms";
 //      mainContainer.appendChild(div); 
 //    }
  
